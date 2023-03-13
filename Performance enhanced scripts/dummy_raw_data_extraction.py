bf85db43fa47ba1bf10cccada0b8f7137a3ac7c6
Jess Farr <j.farr@victorchang.edu.au>
2023-03-13 16:27:42 +1100

    pe

--- Performance enhanced scripts/dummy_raw_data_extraction.py
+++ Performance enhanced scripts/dummy_raw_data_extraction.py
@@ -97,7 +97,7 @@ class PlateWidget():
         num_cpus = int(multiprocessing.cpu_count())
         # num_cpus = 1
         print(num_cpus)
-        pool = multiprocessing.Pool(4)
+        pool = multiprocessing.Pool(num_cpus)
         data = pool.starmap(work, zip(wellIDs, itertools.repeat(num_samples), itertools.repeat(num_sweeps), itertools.repeat(self.json_data), itertools.repeat(self.input_folder), itertools.repeat(start_index), itertools.repeat(end_index)))
 
         count = 0
