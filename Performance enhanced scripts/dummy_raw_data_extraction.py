98099de3c0f586ab6f93221448a02578fd093c9c
Jess Farr <j.farr@victorchang.edu.au>
2023-03-24 09:53:19 +1100

    extraction enhanced

--- Performance enhanced scripts/dummy_raw_data_extraction.py
+++ Performance enhanced scripts/dummy_raw_data_extraction.py
@@ -246,7 +246,7 @@ def main():
     p = PlateWidget(input_folder)
 
     tic = time.time()
-    #high_throughput_ssDeact(os.path.join('/datadrive','syncropatch','Clinical_variant_Brett',), '01092022_AN', p.well_widgets, c, p.json_data.row_count, p.json_data.column_count, 'ssDeact Fit')
+    high_throughput_ssDeact(os.path.join('/datadrive','syncropatch','Clinical_variant_Brett',), '01092022_AN', p.well_widgets, c, p.json_data.row_count, p.json_data.column_count, 'ssDeact Fit')
     #high_throughput_ssAct(os.path.join('/mnt', 'syncropatch', 'Clinical_variant_Brett', ), '01092022_AN', p.well_widgets, c, p.json_data.row_count, p.json_data.column_count, 'ssAct')
     #high_throughput_onset_inact(os.path.join('/mnt', 'syncropatch', 'Clinical_variant_Brett', ), '01092022_AN', p.well_widgets, c, p.json_data.row_count, p.json_data.column_count, 'Onset')
 
