191523592a782fa874eb8dd785a9ee7b177b29d9
Jess Farr <j.farr@victorchang.edu.au>
2023-03-24 13:36:18 +1100

    extraction enhanced

--- Performance enhanced scripts/ssDeact_fit.py
+++ Performance enhanced scripts/ssDeact_fit.py
@@ -623,12 +623,10 @@ def ssDeact_fit(time_secs, data, sweep_pass_qc_array, num_sweeps, wellID, rsq_th
     print(wellID)
 
     for sweep in range(0, num_sweeps):
-        print(sweep)
         if sweep_pass_qc_array[sweep] == 0:
             continue
         actual_sweep = sweep+1
         print('wellID =', wellID, 'sweep = ', sweep, 'process =', os.getpid())
-        print(os.getpid())
         sweep_voltage = voltage_array[sweep]
 
         if sweep_voltage == -80 or sweep_voltage == -90:
