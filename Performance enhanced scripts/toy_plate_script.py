7545b6bd8a84d59be2195eb318d27a85fa144b85
Jess Farr <j.farr@victorchang.edu.au>
2023-03-29 16:49:58 +1100

    pe

---
+++ Performance enhanced scripts/toy_plate_script.py
@@ -0,0 +1,45 @@
+
+
+
+
+import numpy as np
+from scipy import optimize
+
+
+def straight_line(time, m, b):
+    return time*m+b
+
+
+def work(time, data):
+    params, cov = optimize.curve_fit(straight_line, time, data)
+
+
+def toy_plate_script(num_rows, num_cols):
+    data = []
+    time_secs = []
+
+
+    for row in range(0, num_rows):
+        for col in range(0, num_cols):
+            y_data = np.arange(0, 10000, 10000)
+
+            x_data = np.arange(0, 10000, 10000)
+            data.append(y_data)
+            time_secs.append(x_data)
+
+    from concurrent.futures import ProcessPoolExecutor
+
+    with ProcessPoolExecutor(max_workers=20) as executor:
+        # with multiprocessing.pool.ThreadPool(20) as pool:
+        # call a function on each item in a list and process results
+        for result in executor.map(work, time_secs, data):
+            print(result)
+
+def main():
+    toy_plate_script(16, 24)
+
+if __name__ == '__main__':
+    main()
+
+
+
