8da48f63396a6b408bdc2be240810f8c55b5c617
Jess Farr <j.farr@victorchang.edu.au>
2023-03-30 14:33:11 +1100

    pe

--- Performance enhanced scripts/KCNH2_ssDeact_HT.py
+++ Performance enhanced scripts/KCNH2_ssDeact_HT.py
@@ -206,7 +206,7 @@ def high_throughput_ssDeact(parent_dir, plate_name, well_widgets, control_widget
     import multiprocessing
 
     num_cpus = int(multiprocessing.cpu_count())
-    used_cores = 6
+    used_cores = 8
     pool2 = multiprocessing.Pool(processes=used_cores)
     print(used_cores)
     neg_iterables = pool2.starmap(work2, zip(time_secs, data, sweep_pass_qc_array, num_sweeps, wellIDs, itertools.repeat(control_widget.rsq_thresh), itertools.repeat(control_widget.summary_sweep_voltage), itertools.repeat(control_widget.amp_thresh), itertools.repeat(control_widget.cursor_start), itertools.repeat(control_widget.cursor_end)))
