cdca073d1815a61cbbc47948b8e2af3875878a30
Jess Farr <j.farr@victorchang.edu.au>
2023-03-30 15:54:19 +1100

    pe

--- Performance enhanced scripts/dummy_raw_data_extraction.py
+++ Performance enhanced scripts/dummy_raw_data_extraction.py
@@ -251,7 +251,7 @@ def main():
     #c = ControlWidget('ssAct')
     #c = ControlWidget('Onset')
 
-    used_cores = 14
+    used_cores = 15
     p = PlateWidget(input_folder, used_cores)
 
     tic = time.time()
