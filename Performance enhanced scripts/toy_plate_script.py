70a561d5f9bc2d221d872edb5c3340dafa720fcc
Jess Farr <j.farr@victorchang.edu.au>
2023-03-30 12:22:40 +1100

    pe

--- Performance enhanced scripts/toy_plate_script.py
+++ Performance enhanced scripts/toy_plate_script.py
@@ -19,7 +19,10 @@ def straight_line(x, m, b):
 def work(time, data):
     warnings.filterwarnings('ignore')
     #params = optimize.curve_fit(double_exponential, time, data, maxfev=50000, loss='soft_l1', f_scale=0.1, method='trf')
-    params = optimize.curve_fit(double_exponential, time, data, method='trf') #BAD!!
+    try:
+        params = optimize.curve_fit(double_exponential, time, data, method='trf') #BAD!!
+    except:
+        return np.nan
     #params, cov = optimize.curve_fit(double_exponential, time, data,  maxfev=50000) #1.88 seconds, 1.671149492263794
     #print(len(params))
     model = double_exponential(time, params[0], params[1], params[2], params[3], params[4])
